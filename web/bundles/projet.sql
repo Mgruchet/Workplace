904c3796e0dcc86bf0301a00e74500415a14fdff
Maxime Gruchet <maxime.gruchet@gmail.com>
2017-04-05 11:02:09 +0200

    2nd commit

--- web/bundles/projet.sql
+++ web/bundles/projet.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Dim 19 Février 2017 à 23:26
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client :  localhost:8889
+-- Généré le :  Mar 14 Mars 2017 à 16:54
+-- Version du serveur :  5.5.42
+-- Version de PHP :  7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `projet`
@@ -26,15 +20,13 @@ SET time_zone = "+00:00";
 -- Structure de la table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `projet_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_3AF34668C18272` (`projet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `projet_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -42,17 +34,16 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Structure de la table `clients`
 --
 
-CREATE TABLE IF NOT EXISTS `clients` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `ville` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `code_postal` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `tel` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `tel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,17 +51,15 @@ CREATE TABLE IF NOT EXISTS `clients` (
 -- Structure de la table `devis`
 --
 
-CREATE TABLE IF NOT EXISTS `devis` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `devis` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `reference` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `date_envoie` datetime NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `projet_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_8B27C52BC18272` (`projet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `projet_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -78,17 +67,15 @@ CREATE TABLE IF NOT EXISTS `devis` (
 -- Structure de la table `facture`
 --
 
-CREATE TABLE IF NOT EXISTS `facture` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `facture` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `reference` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `date_envoie` datetime NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `projet_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_FE866410C18272` (`projet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `projet_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -96,18 +83,15 @@ CREATE TABLE IF NOT EXISTS `facture` (
 -- Structure de la table `projet`
 --
 
-CREATE TABLE IF NOT EXISTS `projet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `projet` (
+  `id` int(11) NOT NULL,
   `avancement` tinyint(3) NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `clients_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_50159CA9AB014612` (`clients_id`),
-  KEY `IDX_50159CA9A76ED395` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -115,17 +99,14 @@ CREATE TABLE IF NOT EXISTS `projet` (
 -- Structure de la table `tache`
 --
 
-CREATE TABLE IF NOT EXISTS `tache` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tache` (
+  `id` int(11) NOT NULL,
   `designation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `quantite` decimal(10,0) NOT NULL,
   `unite` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `facture_id` int(11) DEFAULT NULL,
-  `devis_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_938720757F2DEE08` (`facture_id`),
-  KEY `IDX_9387207541DEFADA` (`devis_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `devis_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -133,12 +114,14 @@ CREATE TABLE IF NOT EXISTS `tache` (
 -- Structure de la table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `tel` int(11) NOT NULL,
   `enabled` tinyint(1) NOT NULL,
   `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -146,20 +129,116 @@ CREATE TABLE IF NOT EXISTS `user` (
   `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_requested_at` datetime DEFAULT NULL,
   `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_8D93D64992FC23A8` (`username_canonical`),
-  UNIQUE KEY `UNIQ_8D93D649A0D96FBF` (`email_canonical`),
-  UNIQUE KEY `UNIQ_8D93D649C05FB297` (`confirmation_token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `cp` int(11) NOT NULL,
+  `town` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `country` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `birthday` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
-(1, 'test', 'test', 'test@test.com', 'test@test.com', 1, NULL, '$2y$13$35SYGUc6BGzR0NCsaZRzM.CYstKYYPwP5YIw.04VyVPUPPSF5IaHu', '2017-02-19 18:32:43', NULL, NULL, 'a:0:{}'),
-(2, 'maxime', 'maxime', 'max.g@gmail.com', 'max.g@gmail.com', 1, NULL, '$2y$13$JVN8/hZikiK7mEP78RFeJezGvnqu.4YXzJmYlvzjWakWr89voRwcK', '2017-02-15 17:17:50', NULL, NULL, 'a:0:{}');
+INSERT INTO `user` (`id`, `username`, `username_canonical`, `firstname`, `email`, `email_canonical`, `tel`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`, `adresse`, `cp`, `town`, `country`, `birthday`) VALUES
+(1, 'test', 'test', 'testi', 'test@test.com', 'test@test.com', 680680383, 1, NULL, '$2y$13$35SYGUc6BGzR0NCsaZRzM.CYstKYYPwP5YIw.04VyVPUPPSF5IaHu', '2017-03-14 16:30:06', NULL, NULL, 'a:0:{}', '20 rue st sixte', 56220, 'limerzel', 'France', '1993-08-12'),
+(2, 'maxime', 'maxime', '', 'max.g@gmail.com', 'max.g@gmail.com', 0, 1, NULL, '$2y$13$JVN8/hZikiK7mEP78RFeJezGvnqu.4YXzJmYlvzjWakWr89voRwcK', '2017-02-15 17:17:50', NULL, NULL, 'a:0:{}', '', 0, '', '', '0000-00-00');
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_3AF34668C18272` (`projet_id`);
+
+--
+-- Index pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `devis`
+--
+ALTER TABLE `devis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_8B27C52BC18272` (`projet_id`);
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_FE866410C18272` (`projet_id`);
+
+--
+-- Index pour la table `projet`
+--
+ALTER TABLE `projet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_50159CA9AB014612` (`clients_id`),
+  ADD KEY `IDX_50159CA9A76ED395` (`user_id`);
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_938720757F2DEE08` (`facture_id`),
+  ADD KEY `IDX_9387207541DEFADA` (`devis_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D64992FC23A8` (`username_canonical`),
+  ADD UNIQUE KEY `UNIQ_8D93D649A0D96FBF` (`email_canonical`),
+  ADD UNIQUE KEY `UNIQ_8D93D649C05FB297` (`confirmation_token`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `devis`
+--
+ALTER TABLE `devis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `projet`
+--
+ALTER TABLE `projet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
@@ -195,7 +274,3 @@ ALTER TABLE `projet`
 ALTER TABLE `tache`
   ADD CONSTRAINT `FK_9387207541DEFADA` FOREIGN KEY (`devis_id`) REFERENCES `devis` (`id`),
   ADD CONSTRAINT `FK_938720757F2DEE08` FOREIGN KEY (`facture_id`) REFERENCES `facture` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
